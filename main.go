88be8579bc44387dc1a2eba2c17bf4a629cffdd6
Matteo Bjornsson <matteobjornsson@gmail.com>
2024-04-04 10:12:00 -0400

    Configure program with env vars

--- main.go
+++ main.go
@@ -5,40 +5,23 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
-
-	"github.com/redis/go-redis/v9"
 )
 
-func NewRedisClient(
-	address string,
-	password string,
-	database int,
-) *redis.Client {
-	rdb := redis.NewClient(
-		&redis.Options{
-			Addr:     address,
-			Password: password,
-			DB:       database,
-		})
-	return rdb
-}
-
 func main() {
-	config, err := ReadConfig()
+	rdb, err := NewRedisClient()
 	if err != nil {
-		log.Fatalf("Error reading config: %v", err)
+		log.Fatalf("Error creating Redis client: %v", err)
 	}
-	rdb := NewRedisClient(config.RedisAddress, config.RedisPassword, config.RedisDatabase)
 
 	ctx := context.Background()
-	// Configure Redis to send notifications for key events.
+	// Configure Redis to send notifications for keyspace events.
 	_, err = rdb.Do(
 		ctx, "CONFIG", "SET", "notify-keyspace-events", "KEA",
 	).Result()
 	if err != nil {
 		log.Fatalf("Error configuring Redis: %v", err)
 	}
-	// subscribe to the key event channel for set events.
+	// subscribe to the key event channel for "set" events.
 	keyEvent := rdb.PSubscribe(ctx, "__keyevent@0__:set").Channel()
 	// log the key and value for each set event.
 	for {
